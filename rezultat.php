3c8e98232e258d653eaa0463927d5c1d36a418e4
Krunoslav Husak <krunoslav.husak@skole.hr>
2017-11-15 12:55:22 +0100

    Dodali smo nove datoteke

---
+++ rezultat.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <?php 
+    print_r($_POST);
+    echo('Pozdrav, ' . $_POST['prezime']);
+    echo('<br>');
+    echo('Vi imate: ' . $_POST['godine'] . ' godina');
+    echo('<br>');
+    
+    if ($_POST['godine'] >= 18)
+    {
+        echo('Vi ste punoljetni!');
+    }
+    else
+    {
+        echo('Vi niste punoljetni!');
+    }
+    echo(gettype($_POST['godine']));
+
+    echo($_POST['godine'] == 18);
+    ?>
+</body>
+
+</html>
---
+++ unos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Unos</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <form action="rezultat.php" method="post">
+        Ime:<br>
+        <input type="text" name="ime"><br>
+
+        Prezime:<br>
+        <input type="text" name="prezime"><br>
+
+        Godine:<br>
+        <input type="text" name="godine"><br>
+
+        <input type="submit" value="Pošalji!">        
+    </form>
+</body>
+
+</html>
